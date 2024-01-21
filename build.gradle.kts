46d56ea2c9add6363c7a78f4d6684c7c526e0440
Júlio Cesar Bueno Cotta <julio.buenocotta@veepee.com>
2024-01-21 21:36:23 +0100

    project failed to compile with k2 with this config, wait for compose support to kotlin 2.0.0-Beta3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,18 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("com.android.library") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id ("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0-Beta3" apply false
+    id("com.google.devtools.ksp") version "2.0.0-Beta3-1.0.17" apply false
+}
+
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs += listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+            )
+        }
+    }
 }
