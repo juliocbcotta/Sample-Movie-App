e24099172593fb8355ed4cc571bd29723e856b7c
Júlio Cesar Bueno Cotta <julio.buenocotta@veepee.com>
2024-01-22 18:10:14 +0100

    add my first plugin for android libraries

---
+++ plugins/convention/src/main/java/com/android/sample/app/plugins/AndroidLibraryPlugin.kt
@@ -0,0 +1,45 @@
+package com.android.sample.app.plugins
+
+import com.android.build.api.dsl.LibraryExtension
+import com.android.sample.app.plugins.utils.libs
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidLibraryPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.android.library")
+
+            val extension = extensions.getByType<LibraryExtension>()
+            with(extension) {
+                compileSdk = 34
+
+                defaultConfig {
+                    minSdk = 24
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+
+                buildTypes {
+                    release {
+                        isMinifyEnabled = false
+                        proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+                    }
+                }
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_1_8
+                    targetCompatibility = JavaVersion.VERSION_1_8
+                }
+                buildFeatures {
+                    compose = true
+                }
+                composeOptions {
+                    kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
+                }
+            }
+        }
+    }
+}
